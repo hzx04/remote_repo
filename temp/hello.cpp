ebe4de7fbc53292bfb3db3e66435c321ddea5304
yun <24s058019@gmail.com>
2025-08-14 22:42:22 +0800

    m7

---
+++ temp/hello.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;    
+int main(){
+    cout<<"hello world"<<endl;
+    return 0;
+}
+// g++ -o hello hello.cpp -std=c++14
+// ./hello > hello.log
+// cat hello.log
+// git add .
+// git commit -m "first commit"
+// git push -u origin main      
